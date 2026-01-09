932e1247c7b35f4f1299fa4c7f626371d9cf71a3
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-01-09 11:49:10 +0530

    Create the Django App(Studnets)

--- SchoolManagment/settings.py
+++ SchoolManagment/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'Students'
 ]
 
 MIDDLEWARE = [
